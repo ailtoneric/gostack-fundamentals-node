3fd13f3200a40e7e0c8b5fa4f9579915cbfe05ec
Ailton Oliveira <ailton.oliveira@vocacao.org.br>
2020-06-24 18:05:24 -0300

    challenge resolution

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const initialValue = 0;
+
+    const income = this.transactions.reduce(
+      (total, transaction) =>
+        total + (transaction.type === 'income' ? transaction.value : 0),
+      initialValue,
+    );
+
+    const outcome = this.transactions.reduce(
+      (total, transaction) =>
+        total + (transaction.type === 'outcome' ? transaction.value : 0),
+      initialValue,
+    );
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
